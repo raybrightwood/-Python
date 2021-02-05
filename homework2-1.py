0363f889aa6c56f07901f508f79bf4e92ebbcc41
raybrightwood <golubtsov.sa@gmail.com>
2021-02-05 17:17:26 +0100

    Add files via upload

---
+++ homework2-1.py
@@ -0,0 +1,4 @@
+list = ['text', 2, 35.6, None]
+print(list)
+for el in list:
+    print(type(el))
---
+++ homework2-3.py
@@ -0,0 +1,20 @@
+month_number = int (input("Введите номер месяца"))
+
+winter = [12, 1, 2]
+spring = [3, 4, 5]
+summer = [6, 7, 8]
+autumn = [9, 10, 11]
+
+for el in winter:
+    if el == month_number:
+        print ("winter")
+for el in spring:
+    if el is month_number:
+        print ("spring")
+for el in summer:
+    if el is summer:
+        print ("summer")
+
+
+
+
